20566db57c883d8dc34bb0b4eaf069d87a18dc1b
imshubhamkumar <shubhamk773@gmail.com>
2021-04-26 13:25:38 +0530

    Production code added

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ mongoose
 const server = http.createServer(app)
 app.use(express.json());
 app.use("/users", AppRoutes);
-if(process.env.NODE_ENV!=="production"){
+if(process.env.NODE_ENV=="production"){
   app.use(express.static('client/build'))
   const path = require('path')
   app.get("*",(req,res)=>{
