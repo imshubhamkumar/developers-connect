b63adbfdb9a1d0d2e830e3b73ec9c55de84140cb
imshubhamkumar <shubhamk773@gmail.com>
2021-04-29 11:30:55 +0530

    config file updeted

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
         API_URL: '',
-        SCERET: process.env.JWT_SCERET
+        SCERET: 'kCINp085KsigJTBPCoxm'
     }
 } else {
     module.exports = {
